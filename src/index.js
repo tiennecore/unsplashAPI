0555b5af6ee56ae800a10574acab3b025aeeb38b
tiennecore <wvetienne@gmail.com>
2022-01-08 17:53:42 +0100

    change setence

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+/*
+REDUX => npm i react-redux redux redux-devtools-extension redux-thunk
+
+const store = createStore(
+    reducers,
+    composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>
+  ,
+  document.getElementById('root')
+);
+*/
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +29,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h1>Quick React Project by Tiennecore</h1>
+            <span className="description">
+                Tienne'codes<a href="https://github.com/etiennepuig/quick_launch" target=""> quick starter
+                launcher </a>running on react, sass.
+            </span>
+            <h1>Hey let's code</h1>
+        </div>
+    );
+};
+
+export default Home;
